74de02f0520486fc03ca9a1c858d8d58f255958b
Alexandre <fernandes.alexandre@aluno.ifsp.edu.br>
2026-09-12 19:53:33 -0300

    Initialize Flask app and register main controller

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
+from flask import Flask
+from controllers.main_controller import main
 
+app = Flask(__name__)
+
+# Registra as rotas do Controller
+app.register_blueprint(main)
+
+if __name__ == "__main__":
+    app.run(debug=True)
